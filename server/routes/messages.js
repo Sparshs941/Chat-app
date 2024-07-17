d0e3ecb2653fb90d828063c3ee0425231064161f
Sparsh Sharma <100956477+Sparshs941@users.noreply.github.com>
2024-07-17 18:07:55 +0530

    Add files via upload

---
+++ server/routes/messages.js
@@ -0,0 +1,8 @@
+
+const { addMessage, getMessages } = require("../controllers/messageController.js");
+const router = require("express").Router();
+
+router.post("/addmsg/", addMessage);
+router.post("/getmsg/", getMessages);
+
+module.exports = router;
